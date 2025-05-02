4446496ee511e07f4a305f65ab38938f88ef66f2
Vaquicraft <nicolasbondenet@gmail.com>
2025-05-02 21:54:27 +0200

    DES BALISES CENTER !!!!!

--- connexion.php
+++ connexion.php
@@ -6,46 +6,62 @@ if (session_status() == PHP_SESSION_NONE)
 ?>
 
 <!DOCTYPE html>
-	<html>
-	<head>
-	<meta charset="utf8" />
-	<title> Naruto Project </title>
-	<link rel="stylesheet" href="style.css" />
-	</head>
-	<body>
-	
-			 <a href="inscription.php">S'inscrire</a>
-	
-	<header>
-<center> <h1> <b> Naruto Project </center> </h1> </b> <br />
-	</header>
-	
-		<body>
-	<header>
-	<h3> <center> Connexion </h3> </center>
-	</header>
-	
-	
-	
-	<section>
-	<center>
-	<form method="post" action="traitement_connexion.php">
-	<p><input type="text" name="loginco" placeholder="Pseudo" required /></p>
-	<p><input type="password" name="passwordco" placeholder="Mot de passe" required /></p>
-	<p><input type="submit" value="Se connecter" /></p>
-	<form method="post" action="inscription.php">
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>The Wheel of Fight</title>
+	<link rel="stylesheet" href="newstyle.css" />
+</head>
+<body>
 
-	
-	</section>
-	</body>
+	<header class="header">
+		<ul class="headerMenu">
+			<li class="headerMenuConnexion">
+				<a href="connexion.php">Se connecter</a>
+			</li>
+
+			<li class="headerMenuRegister">
+				<a href="inscription.php">S'inscrire</a>
+			</li>
+
+			<li class="headerMenuAbout">
+				<a href="about.php">À Propos</a>
+			</li>
+		</ul>
+	</header>
+
+	<div class="connexionMain">
+		<h1 class="connexionMainTitle">
+			The Wheel of Fight
+		</h1>
+
 		
-	<section>	
 
-	</center>
+	</div>
 
-	</body>
-	</html>
-	
+	<div class="connexionForm">
+		<form method="post" action="traitement_connexion.php"></form>
+
+		<div class="connexionUsernameForm">
+			<label class="formLabel" for="username">Nom d'utilisateur</label>
+			<input class="formField" id="username" type="text" required>
+		</div>
+
+		<div class="connexionPasswordForm">
+			<label class="formLabel" for="password">Mot de Passe</label>
+			<input class="formField" id="password" type="password" required>
+		</div>
+
+		<div class="connexionSubmitForm">
+			<input class ="formSubmit" type="submit" value="Se connecter" />
+		</div>	
+
+	</div>
+
+</body>
+</html>
+
 	
 
 	
