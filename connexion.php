da824bba057a07f96473a7eebbc31ebfd2022cb2
Vaquicraft <nicolasbondenet@gmail.com>
2025-05-04 14:02:17 +0200

    update

--- connexion.php
+++ connexion.php
@@ -1,69 +1,17 @@
 <?php
-if (session_status() == PHP_SESSION_NONE)
+
+
+if (isset($_SESSION['login']))
 {
-	session_start();
+	header('location: membre.php');
 }
+
+else
+{
+	require 'formulaire_connexion.php';
+}
+
 ?>
-
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>The Wheel of Fight</title>
-	<link rel="stylesheet" href="newstyle.css" />
-</head>
-<body>
-
-	<header class="header">
-		<ul class="headerMenu">
-			<li class="headerMenuConnexion">
-				<a href="connexion.php">Connexion</a>
-			</li>
-
-			<li class="headerMenuRegister">
-				<a href="inscription.php">S'inscrire</a>
-			</li>
-
-			<li class="headerMenuAbout">
-				<a href="about.php">À Propos</a>
-			</li>
-		</ul>
-	</header>
-
-	<div class="connexionMain">
-		<h1 class="connexionMainTitle">
-			The Wheel of Fight
-		</h1>
-
-		
-
-	</div>
-
-	<div class="connexionForm">
-		<h2 class="connexionSubTitle">
-			Connexion
-		</h2>
-		<form method="post" action="traitement_connexion.php"></form>
-
-		<div class="connexionUsernameForm">
-			<label class="formLabel" for="username">Nom d'utilisateur</label>
-			<input class="formField" id="username" type="text" required>
-		</div>
-
-		<div class="connexionPasswordForm">
-			<label class="formLabel" for="password">Mot de Passe</label>
-			<input class="formField" id="password" type="password" required>
-		</div>
-
-		<div class="connexionSubmitForm">
-			<input class ="formSubmit" type="submit" value="Se connecter" />
-		</div>	
-
-	</div>
-
-</body>
-</html>
 
 	
 
--- traitement_connexion.php
+++ traitement_connexion.php
@@ -1,17 +1,25 @@
 <?php
+
+if (isset($_SESSION['login']))
+{
+	header('location: membre.php');
+}
+
+
 require 'functions.php';
 check_login();
 bdd_connexion();
+require 'connexion.php';
 
-	$hache = sha1($_POST['passwordco']);			   
+	$hache = sha1($_POST['pass']);			   
 	$req=$pdo->prepare('SELECT * FROM users WHERE login=?');
-	$req->execute([$_POST['loginco']]);
+	$req->execute([$_POST['login']]);
 	$donnees=$req->fetch();
 				
-	if (!$donnees || !password_verify($_POST['passwordco'], $donnees['password']))
+	if (!$donnees || !password_verify($_POST['pass'], $donnees['password']))
 	{
-		echo 'Mauvais identifants <html> <br /> <a href="index.php">Retour </html>';
-		// todo fix
+		echo 'Mauvais identifants';
+		die;
 	}
 		
 	else
